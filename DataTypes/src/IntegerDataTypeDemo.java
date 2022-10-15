cab01ed2ccc266af65b471d9be954853bf65950b
sr1983-it-gl <100977885+sr1983-it-gl@users.noreply.github.com>
2022-10-15 18:29:28 +0530

    Add files via upload

---
+++ DataTypes/src/IntegerDataTypeDemo.java
@@ -0,0 +1,13 @@
+
+public class IntegerDataTypeDemo {
+
+	public static void main(String[] args) {
+		
+		int numberOfEmployess = 2_90_000;
+		System.out.println(numberOfEmployess);
+		
+		
+		numberOfEmployess = 290000;
+		System.out.println(numberOfEmployess);
+	}
+}
